aede7208441432a45530def3f41d8607bd2396ba
Alfredo Martinez <xshaffter@gmail.com>
2021-11-25 05:55:51 -0700

    [UPDATE] no security

--- api/views/search.py
+++ api/views/search.py
@@ -10,6 +10,7 @@ from cat.models import Search, ExchangeValue, TIIESearch
 class SearchViewSet(viewsets.ModelViewSet):
     queryset = Search.objects.all()
     serializer_class = SearchSerializer
+    permission_classes = []
 
     @transaction.atomic
     def create(self, request, *args, **kwargs):
